f6f45b6532da0eea24493e4a2d6f5cfed087f697
putian9935 <64566589+putian9935@users.noreply.github.com>
2021-08-26 22:26:08 +0800

    add calibrator for init

--- calibrator.py
+++ calibrator.py
@@ -4,7 +4,16 @@ Calibrate the laser response to DAC output
 
 import time 
 import numpy as np
-from scipy.interpolate import interp1d 
+from matplotlib.widgets import Cursor
+import matplotlib.pyplot as plt 
+
+def ls_reg(xarr, yarr):
+    xmean = np.mean(xarr)
+    ymean = np.mean(yarr)
+    slope = np.sum((xarr-xmean)*(yarr-ymean)) / np.sum((xarr-xmean)**2) 
+    intercept = ymean - slope * xmean 
+    print('Intercept is', intercept)
+    return lambda _: slope * _ + intercept 
 
 
 class Calibrator: 
@@ -12,7 +21,7 @@ class Calibrator:
         self.write_dac = write_dac
         self.read_wlm = read_wlm 
     
-    def calibrate(self, num=100):
+    def calibrate(self, num=100, repeat=1):
         v = np.linspace(-3,3,num)
         wl = []
 
@@ -21,5 +30,38 @@ class Calibrator:
             time.sleep(.1)
             wl.append(self.read_wlm())
         
-        return interp1d(v, wl) 
+        # wlv = interp1d(v, wl) 
+        wl = np.array(wl)
+        self.fig, self.ax = plt.subplots()
+        self.ax.plot(v, wl)
+        x1, x2 = self.retrieve_x_limit()
+        mask = (v > x1) & (v < x2) 
+        reg_func = ls_reg(v[mask], wl[mask])
+
+        _, ax = plt.subplots()
+        ax.plot(v, wl)
+        ax.plot([.9*x1, 1.1*x2], [reg_func(.9*x1), reg_func(1.1*x2)], '--')
+        plt.show()
+
+
+    def retrieve_x_limit(self):
+        vlines = []
+        def add_cursor(fig, ax,):
+            cursor = Cursor(ax, useblit=True, horizOn=False, color='red', linewidth=2)
+
+            def onclick(event):
+                vlines.append(ax.axvline(x=event.xdata, color='k'))
+                plt.pause(.2)  # have to flush the plot 
+                
+            fig.canvas.mpl_connect('button_press_event', onclick)
+            
+            return cursor  # cursor has to be returned, otherwise destroyed (due to zero ref count)
+        
+        _ = add_cursor(self.fig, self.ax)
+        plt.show()
+        
+        x1, x2 = vlines[-2].get_xdata()[0], vlines[-1].get_xdata()[0]
+        if x1 > x2: 
+            x1, x2 = x2, x1 
+        return x1, x2
         
