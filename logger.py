cb6d2004a53634618d44fe0b3790650fda12af76
putian9935 <64566589+putian9935@users.noreply.github.com>
2021-08-26 09:55:29 +0800

    add logger

---
+++ logger.py
@@ -0,0 +1,17 @@
+import time 
+
+class Logger():
+    def __init__(self, data=None, fname=None):
+        if not fname:
+            fname = 'log@%s.txt'%time.strftime("%Y-%m-%d-%H%M%S", time.localtime())
+        self.file = open(fname, 'w') 
+
+        for line in data: 
+            self.log(line)
+    
+    def log(self, line):
+        self.file.write(', '.join(str(_) for _ in line))
+        self.file.write('\n')
+
+
+
