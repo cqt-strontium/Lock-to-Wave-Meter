83691888170f837d583a938931fc95a7bafb5b16
putian9935 <64566589+putian9935@users.noreply.github.com>
2021-09-29 10:01:20 +0800

    fix bug
    error with default ctor

--- logger.py
+++ logger.py
@@ -1,26 +1,24 @@
-import time 
+import time
 
 class Logger():
     def __init__(self, data=None, fname=None, header=None):
         if not fname:
             fname = 'log@%s.txt'%time.strftime("%Y-%m-%d-%H%M%S", time.localtime())
-        
-        self.file = open(fname, 'w') 
+
+        self.file = open(fname, 'w')
 
         if header:
             self.file.write(header)
-            
-        for line in data: 
-            self.log(line)
-    
+
+        if data:
+            for line in data: 
+                self.log(line)
+
     def log(self, line):
         self.file.write(', '.join(str(_) for _ in line))
         self.file.write('\n')
         self.file.flush()
-    
+
 
     def cleanup(self):
         self.file.close()
-
-
-
